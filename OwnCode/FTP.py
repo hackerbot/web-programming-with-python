a065ec40543cf307989033ec78ed9fea1587ce05
hackerbot <ninh@ninh-macbookpro2.roam.corp.google.com>
2015-05-04 11:58:53 -0700

    Section 5 - FTP
    File Transfer Protocol Handling

---
+++ OwnCode/FTP.py
@@ -0,0 +1,31 @@
+#ftp 'ftp.url.com'
+#dir
+#put filename
+
+def getdir():
+    import ftplib
+    connect = ftplib.FTP('ftp.url.com')
+    connect.login('administrator\admin', 'password')
+    data = []
+    connect.dir(data.append)
+    connect.quit()
+    for line in data:
+        print(line)
+
+def getfile():
+    import ftplib
+    import sys
+    filename = sys.argv[1]
+    connect = ftplib.FTP('ftp.url.com')
+    connect.login('administrator\admin', 'password')
+    connect.retrlines('RETR ' + filename)
+    connect.quit()
+
+def putfile():
+    import ftplib
+    import sys
+    connect = ftplib.FTP('ftp.url.com')
+    connect.login('administrator\admin', 'password')
+    file = open(filename, 'rb')
+    connect.storebinary('STOR ' + filename, file)
+    connect.quit()    
